f500924b4fc3639dc7ffeaf038cfb45a19c79887
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-02 12:34:02 +0200

    solution of avarageand total population

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write your code here
+const population = [...document.querySelectorAll('.population')].map(
+  (el) => el.textContent,
+);
+
+function totalSumOfPopulation(piople) {
+  let totalSumPopulation = 0;
+
+  piople.map((el) => {
+    totalSumPopulation += Number(el.replace(/,/g, ''));
+  });
+
+  return totalSumPopulation;
+}
+
+function getAvaragePopulation(piople) {
+  let averageSumPopulation = 0;
+
+  piople.map((el) => {
+    averageSumPopulation += Number(el.replace(/,/g, ''));
+  });
+
+  return Math.floor(averageSumPopulation / piople.length);
+}
+
+document.querySelector('.total-population').textContent =
+  totalSumOfPopulation(population);
+
+document.querySelector('.average-population').textContent =
+  getAvaragePopulation(population);
